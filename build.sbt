6a54631d34e625ec5422c1efd44ce74d646d33cd
taintech <rinattainov@gmail.com>
2017-03-22 00:51:43 +0100

    added dukascopy config and example

--- build.sbt
+++ build.sbt
@@ -8,7 +8,12 @@ scalaVersion := "2.12.1"
 
 crossScalaVersions := Seq("2.10.6", "2.11.8", "2.12.1")
 
+resolvers += "Dukascopy" at "https://www.dukascopy.com/client/jforexlib/publicrepo/"
+
 libraryDependencies ++= Seq(
+  "com.dukascopy.dds2" % "DDS2-jClient-JForex" % "3.0.18",
+  "com.dukascopy.api" % "JForex-API" % "2.13.30",
+  "com.typesafe" % "config" % "1.3.1",
   "org.scalatest" %% "scalatest" % "3.0.1" % "test",
   "org.scalacheck" %% "scalacheck" % "1.13.4" % "test"
 )
--- src/main/scala/com/taintech/worldwatch/App.scala
+++ src/main/scala/com/taintech/worldwatch/App.scala
@@ -1,7 +1,19 @@
 package com.taintech.worldwatch
 
+import com.typesafe.config.{Config, ConfigFactory}
+
 object App {
+
+  val conf: Config = ConfigFactory.load()
+
   def main(args: Array[String]) {
     print("Hello basic-project!")
+
+
+    // Load our own config values from the default location, application.conf
+
+    println("The answer is: " + conf.getString("dukascopy-app.test"))
+    println("The answer is: " + conf.getString("dukascopy-app.demo-login"))
+    println("The answer is: " + conf.getString("dukascopy-app.demo-pwd"))
   }
 }
